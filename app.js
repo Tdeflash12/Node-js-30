3d6b4fac2ac591bd3fa6b5464e8d21c8283d03b4
Abhesh Mandal <abheshmandal249@gmail.com>
2024-11-10 13:41:29 +0545

    day 6 autth design

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require("express")
 const app= express()
 //const app = require("express")()
 app.set('view engine','ejs')
+
+app.use(express.static('public/css/')) // used to make file publicin node js that can be used
 app.get("/",(req,res)=>{
     res.render('home')
 })
@@ -10,7 +12,12 @@ app.get("/about",(req,res)=>{
     res.render('about')
 })
 
-
+app.get("/register",(req,res)=>{
+    res.render('auth/register')
+})
+app.get("/login",(req,res)=>{
+    res.render("auth/login")
+})
 
 
 app.listen(3000,()=>{
